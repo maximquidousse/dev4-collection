18172e5a3c302e531f171940853da45df6799aa3
maximquidousse <maxim.quidousse@student.howest.be>
2021-08-07 13:43:25 +0200

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '412710823406702a11aef13fe188a0d5'),
+    },
+  },
+});
