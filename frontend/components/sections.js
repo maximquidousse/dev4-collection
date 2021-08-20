8f7ec2edd1ee6b351e9279459cbe0d23b18c302f
maximquidousse <maxim.quidousse@student.howest.be>
2021-08-20 17:39:01 +0200

    turn off preview

--- frontend/components/sections.js
+++ frontend/components/sections.js
@@ -22,33 +22,33 @@ const Section = ({ sectionData, shoes, user }) => {
   return <SectionComponent data={sectionData} shoes={shoes} user={user} />
 }
 
-const PreviewModeBanner = () => {
-  const router = useRouter()
-  const exitURL = `/api/exit-preview?redirect=${encodeURIComponent(
-    router.asPath
-  )}`
+// const PreviewModeBanner = () => {
+//   const router = useRouter()
+//   const exitURL = `/api/exit-preview?redirect=${encodeURIComponent(
+//     router.asPath
+//   )}`
 
-  return (
-    <div className="py-4 bg-red-600 text-red-100 font-semibold uppercase tracking-wide">
-      <div className="container">
-        Preview mode is on.{" "}
-        <a
-          className="underline"
-          href={`/api/exit-preview?redirect=${router.asPath}`}
-        >
-          Turn off
-        </a>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="py-4 bg-red-600 text-red-100 font-semibold uppercase tracking-wide">
+//       <div className="container">
+//         Preview mode is on.{" "}
+//         <a
+//           className="underline"
+//           href={`/api/exit-preview?redirect=${router.asPath}`}
+//         >
+//           Turn off
+//         </a>
+//       </div>
+//     </div>
+//   )
+// }
 
 // Display the list of sections
 const Sections = ({ sections, preview, shoes, user }) => {
   return (
     <div className="flex flex-col">
       {/* Show a banner if preview mode is on */}
-      {preview && <PreviewModeBanner />}
+      {/* {preview && <PreviewModeBanner />} */}
       {/* Show the actual sections */}
       {sections.map((section) => (
         <Section
