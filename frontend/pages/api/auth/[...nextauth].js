04c439e8d93038d52ea2bd03e6830d2577d86088
maximquidousse <maxim.quidousse@student.howest.be>
2021-08-20 17:18:25 +0200

    test google client id

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+const yo = process.env.GOOGLE_CLIENT_ID
+console.log(yo)
 const options = {
   providers: [
     Providers.Google({
@@ -7,5 +9,5 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-};
-export default (req, res) => NextAuth(req, res, options);
+}
+export default (req, res) => NextAuth(req, res, options)
